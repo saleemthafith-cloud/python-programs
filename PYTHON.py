134387373f1c6ed9ccaa02b227328a2086006136
saleemthafith-cloud <saleemthafith@gmail.com>
2026-09-18 15:31:34 +0530

    Add gender-based name collection functionality
    Collect names based on gender input and store males in a list.

--- PYTHON.py
+++ PYTHON.py
@@ -1,1 +1,11 @@
-
+n=int(input('enter limit:'))
+male=[]
+female=[]
+for i in range(n):
+    name=input('enter your name:')
+    gender=input('enter F/M:')
+    if gender=='M':
+        male.append(name)
+    else:
+        print(name)
+print(male)
